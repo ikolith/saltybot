acfd3298f563f5b1feac34c51eb77a591c432de5
grebe-s <thesloth00@gmail.com>
2020-09-20 11:46:06 -0700

    Added test code
    Basic emoji response

---
+++ saltbot.py
@@ -0,0 +1,24 @@
+import discord
+#import sqlite3
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+token = os.getenv('BOT_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} ready.')
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('!test'):
+        await message.channel.send(':salt:')
+print(token)
+client.run(token)
