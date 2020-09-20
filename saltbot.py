cd89c1cbe8de5fc849ecb48c1eaccc91810efe34
grebe-s <thesloth00@gmail.com>
2020-09-20 14:00:07 -0700

    not elegant, but the while loop for salt send will yield and allow other functions to run

--- saltbot.py
+++ saltbot.py
@@ -1,16 +1,32 @@
 import discord
-#import sqlite3
+import sqlite3
 import os
+import asyncio
+from random import randint
 from dotenv import load_dotenv
 
 
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 client = discord.Client()
+game_channel = 'not set'
+
+async def salt_spawn():
+    while True:
+        await asyncio.sleep(1)
+        #print(game_channel)
+        if game_channel != 'not set':
+            time_to_salt_spawn = randint(2,5)
+            await asyncio.sleep(time_to_salt_spawn)
+            print('send salt')
+            print(time_to_salt_spawn)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} ready.')
+    #await salt_spawn()
+    await salt_spawn()
 
 @client.event
 async def on_message(message):
@@ -20,4 +36,13 @@ async def on_message(message):
 
     if message.content.lower().startswith('!test'):
         await message.channel.send(':salt:')
+    
+    if message.content.lower().startswith('!gamehere'):
+        print('got it')
+        global game_channel
+        game_channel = message.channel
+
+        print(game_channel)
+    #if message.content.lower().startswith('')
+
 client.run(token)
