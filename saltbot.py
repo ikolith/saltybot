2118fd36b51ff1deef28f9dfdda6934d8eece34e
grebe-s <thesloth00@gmail.com>
2020-09-21 13:42:14 -0700

    added check_message to speed up writing new commands

--- saltbot.py
+++ saltbot.py
@@ -1,45 +1,25 @@
-<<<<<<< HEAD
-=======
 #!/usr/bin/env python3
 #
 
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
 import discord
 import sqlite3
 import os
 import asyncio
 from random import randint
-<<<<<<< HEAD
-from dotenv import load_dotenv
-
-
-load_dotenv()
-token = os.getenv('BOT_TOKEN')
-client = discord.Client()
-game_channel = 'not set'
-=======
 from secret_token import BOT_TOKEN
 
 token = BOT_TOKEN
 if(not token):
-    print("no token found. Please create a file called token.py in the same directory as saltybot and put in that file the text: BOT_TOKEN=\"whatever.your.bot.token.is.blah.blah.blah\"")
+    print("no token found. Please create a file called secret_token.py in the same directory as saltybot and put in that file the text: BOT_TOKEN=\"whatever.your.bot.token.is.blah.blah.blah\"")
     print("the rest of the script will attempt to run now, but almost certainly will not work.")
     print()
 client = discord.Client()
 game_channel = ''
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
 
 async def salt_spawn():
     while True:
         await asyncio.sleep(1)
         #print(game_channel)
-<<<<<<< HEAD
-        if game_channel != 'not set':
-            time_to_salt_spawn = randint(2,5)
-            await asyncio.sleep(time_to_salt_spawn)
-            print('send salt')
-            print(time_to_salt_spawn)
-=======
         if game_channel != '':
             time_to_salt_spawn = randint(5,500)
             print(time_to_salt_spawn)
@@ -72,15 +52,12 @@ def print_players():
     cursor.execute("select * from players")
     print(cursor.fetchall()) #could also fetchone if we wanted only one player
     db.close()
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
-
+def check_message(message,cue):
+   return message.content.lower().startswith('!'+ cue)
 
 @client.event
 async def on_ready():
     print(f'{client.user} ready.')
-<<<<<<< HEAD
-    #await salt_spawn()
-=======
     #example code
     try:
         create_database()
@@ -92,33 +69,18 @@ async def on_ready():
     except:
         pass #there's already a player there (mostly because this is dummy example code)
     print_players()
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
     await salt_spawn()
 
 @client.event
 async def on_message(message):
 
     if message.author == client.user:
-<<<<<<< HEAD
-        return
-=======
         return #don't react to our own messages
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
 
-    if message.content.lower().startswith('!test'):
+    if check_message(message,'test'):
         await message.channel.send(':salt:')
     
-    if message.content.lower().startswith('!gamehere'):
-<<<<<<< HEAD
-        print('got it')
-        global game_channel
-        game_channel = message.channel
-
-        print(game_channel)
-    #if message.content.lower().startswith('')
-
-client.run(token)
-=======
+    if check_message(message,'gamehere'):
         print('got it: game_channel = ' + str(message.channel))
         global game_channel
         game_channel = message.channel
@@ -127,23 +89,23 @@ client.run(token)
         #this code could be compressed/refactored.
 #TODO: gosh, we're gonna have to parse, aren't we? yee haw.
 #TODO: also the more elegant system should check game channel and so forth
-    if message.content.lower().startswith('!leanin'):
+    if check_message(message,'leanin'):
         await message.channel.send(message.author.name + " activates Lean In Stance, gaining +1 to offensive moves!")
         #TODO: this doesnt do anything yet lol
 
-    if message.content.lower().startswith('!getout'):
+    if check_message(message,'getout'):
         await message.channel.send(message.author.name + " activates Get Out Stance, gaining +1 to retreative moves!")
         #TODO: this doesnt do anything yet lol
 
-    if message.content.lower().startswith('!playchicken'):
+    if check_message(message,'playchicken'):
         await message.channel.send(message.author.name + " remembers what Herbert the Affectionate Insectoid said about chicken, and runs off a cliff for no reason!")
         #TODO: this should actually let you play chicken
 
-    if message.content.lower().startswith('!saltman'):
+    if check_message(message,'saltman'):
         await message.channel.send(':snowman2:')
-    if message.content.lower().startswith('!jungledog'):
+    if check_message(message,'jungledog'):
         await message.channel.send(':dog2:')
-    if message.content.lower().startswith('!scrip'):
+    if check_message(message,'scrip'):
         #TODO: should require paper and writing utensil (or... any object? put scrip on a dog lol)
         #TODO: this should really be !scrip [object from inventory] [quantity of money or other object]
         await message.channel.send(message.author.name + " writes '$5' on a piece of paper and signs it!")
@@ -151,4 +113,3 @@ client.run(token)
     #if message.content.lower().startswith('')
 
 client.run(token)
->>>>>>> 62db2b012a73dcd1e6ea6b39d87aad977ec8e039
