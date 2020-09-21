898797a6251c42ee76342efffb5216c105a0e774
Mernon Squirnon <mernonsquirnon@gmail.com>
2020-09-21 09:30:35 +0100

    dumb bullshit time

--- saltbot.py
+++ saltbot.py
@@ -9,13 +9,13 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 client = discord.Client()
-game_channel = 'not set'
+game_channel = ''
 
 async def salt_spawn():
     while True:
         await asyncio.sleep(1)
         #print(game_channel)
-        if game_channel != 'not set':
+        if game_channel != '':
             time_to_salt_spawn = randint(2,5)
             await asyncio.sleep(time_to_salt_spawn)
             print('send salt')
@@ -32,17 +32,41 @@ async def on_ready():
 async def on_message(message):
 
     if message.author == client.user:
-        return
+        return #don't react to our own messages
 
     if message.content.lower().startswith('!test'):
         await message.channel.send(':salt:')
     
     if message.content.lower().startswith('!gamehere'):
-        print('got it')
+        print('got it: game_channel = ' + str(message.channel))
         global game_channel
         game_channel = message.channel
 
-        print(game_channel)
+#TODO: we probably want a more elegant dispatch system than string startswith checking
+        #this code could be compressed/refactored.
+#TODO: gosh, we're gonna have to parse, aren't we? yee haw.
+#TODO: also the more elegant system should check game channel and so forth
+    if message.content.lower().startswith('!leanin'):
+        await message.channel.send(message.author.name + " activates Lean In Stance, gaining +1 to offensive moves!")
+        #TODO: this doesnt do anything yet lol
+
+    if message.content.lower().startswith('!getout'):
+        await message.channel.send(message.author.name + " activates Get Out Stance, gaining +1 to retreative moves!")
+        #TODO: this doesnt do anything yet lol
+
+    if message.content.lower().startswith('!playchicken'):
+        await message.channel.send(message.author.name + " remembers what Herbert the Affectionate Insectoid said about chicken, and runs off a cliff for no reason!")
+        #TODO: this should actually let you play chicken
+
+    if message.content.lower().startswith('!saltman'):
+        await message.channel.send(':snowman2:')
+    if message.content.lower().startswith('!jungledog'):
+        await message.channel.send(':dog2:')
+    if message.content.lower().startswith('!scrip'):
+        #TODO: should require paper and writing utensil (or... any object? put scrip on a dog lol)
+        #TODO: this should really be !scrip [object from inventory] [quantity of money or other object]
+        await message.channel.send(message.author.name + " writes '$5' on a piece of paper and signs it!")
+        
     #if message.content.lower().startswith('')
 
 client.run(token)
